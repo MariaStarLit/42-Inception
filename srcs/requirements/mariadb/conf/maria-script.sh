d1499d3e422fcb634ec5c4793c736e95a228a7a0
pedrolinux <pedrocaetano90@gmail.com>
2025-02-26 15:10:05 +0100

    Added wordpress to docker-compose Added initial scripting for both wordpress AND mariadb TO DO: environment variables

--- srcs/requirements/mariadb/conf/maria-script.sh
+++ srcs/requirements/mariadb/conf/maria-script.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-echo "HELLLO!!!"
+echo " --- Setting MariaDB"
+
+service mariadb start
+
+mariadb -u root -e "
+	CREATE DATABASE IF NOT EXISTS MY_DB;
+	CREATE USER IF NOT EXISTS 'MY_USER'@'%' IDENTIFIED BY 'MY_USER_PASS';
+	GRANT ALL PRIVILEGES ON MY_DB.* TO 'MY_USER'@'%';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+	FLUSH PRIVILEGES;
+	"
+
+mariadb -u root -p -e "SHOW DATABASES;"
+
+# mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
--- srcs/requirements/wordpress/conf/wordpess-script.sh
+++ srcs/requirements/wordpress/conf/wordpess-script.sh
@@ -1,2 +1,27 @@
 #!/bin/sh
-echo "HELLLO!!!"
+echo " --- Wordpress Script"
+php -v
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+echo " --- Setting up WP"
+
+wp core download --allow-root
+wp core config \
+    --dbname=globex \
+    --dbuser=globex \
+    --dbpass='password' \
+    --allow-root
+wp core install \
+    --skip-email \
+    --url=https://globex.turnipjuice.media \
+    --title='Globex Corporation' \
+    --admin_user=abe \
+    --admin_email=abe@turnipjuice.media \
+    --admin_password='password' \
+    --allow-root
+
+
