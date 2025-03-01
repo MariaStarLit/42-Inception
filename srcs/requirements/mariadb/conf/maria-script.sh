9920e6cab839f53d27d2f54edbdd325bff242c24
MariaStarLit <mde-avel@DESKTOP-R04921E>
2025-03-01 12:40:12 +0000

    .env - mariadb

--- srcs/requirements/mariadb/conf/maria-script.sh
+++ srcs/requirements/mariadb/conf/maria-script.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
+
+#  if ! pgrep mariadbd > /dev/null; then
+# 	service mariadb start
+# 	sleep 5
+# fi
+
 echo "----- Setting MariaDB -----"
 
- if ! pgrep mariadbd > /dev/null; then
-	service mariadb start
-	sleep 5
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
-fi
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql' &
 
 mariadb -u root -e "
 	CREATE DATABASE IF NOT EXISTS $MY_DB;
 	CREATE USER IF NOT EXISTS '$MY_USER'@'%' IDENTIFIED BY '$MY_USER_PASS';
 	GRANT ALL PRIVILEGES ON $MY_DB.* TO '$MY_USER'@'%';
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MY_ROOT_PASS';
 	FLUSH PRIVILEGES;
 	"
 
+#mysqladmin -u root -p$MY_ROOT_PASS shutdown
 mariadb -u root -p -e "SHOW DATABASES;"
 
-# mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
+wait
