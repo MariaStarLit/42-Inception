807803ad237968ef2931093b08d183dad46d588d
pedrolinux <pedrocaetano90@gmail.com>
2025-03-01 14:17:24 +0100

    small fix for mariadb start

--- srcs/requirements/mariadb/conf/maria-script.sh
+++ srcs/requirements/mariadb/conf/maria-script.sh
@@ -10,7 +10,19 @@ echo "----- Setting MariaDB -----"
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
+# Start the DB if it doesn’t exist
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing database..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
+
 mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql' &
+
+echo "Waiting for MariaDB to start..."
+until mariadb -u root -e "SELECT 1" 2>/dev/null; do
+    sleep 2
+    echo "Still waiting..."
+done
 
 mariadb -u root -e "
 	CREATE DATABASE IF NOT EXISTS $MY_DB;
